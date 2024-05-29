57efa3f7da3c0574c2de4757742ed438489c3735
Shonsu <shonsu82@gmail.com>
2024-05-29 20:27:40 +0200

    add BmiCalculatorFacadeTests

---
+++ MyProject.Tests/BmiCalculatorFacadeTests.cs
@@ -0,0 +1,24 @@
+﻿namespace MyProject.Tests;
+
+public class BmiCalculatorFacadeTests
+{
+    private const string OVERWEIGHT_SUMMARY = "You are a bit overweight";
+
+    [Fact]
+    public void GetResult_ForValidInputs_ReturnCorrectResult()
+    {
+        // arrange
+        IBmiDeterminator bmiDeterminator = new BmiDeterminator();
+        BmiCalculatorFacade facade = new BmiCalculatorFacade(UnitSystem.Metric, bmiDeterminator);
+        double weight = 90;
+        double height = 190;
+
+        // act
+        BmiResult result = facade.GetResult(weight, height);
+
+        // assert
+        Assert.Equal(BmiClassification.Overweight, result.BmiClassification);
+        Assert.Equal(24.93, result.Bmi);
+        Assert.Equal(OVERWEIGHT_SUMMARY, result.Summary);
+    }
+}
