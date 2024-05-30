d68a6b1d9ce446e419a2e5a6bc2817d89f8b2e0e
Shonsu <shonsu82@gmail.com>
2024-05-30 08:34:15 +0200

    add excercises

---
+++ Exercise/Escercise.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Escercise.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Exercise/Exercise/2/ListHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public static class ListHelper
+    {
+        public static List<int> FilterOddNumber(List<int> listOfNumbers)
+        {
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (listOfNumbers[i] % 2 == 0)
+                {
+                    listOfNumbers.RemoveAt(i);
+                }
+            }
+            return listOfNumbers;
+        }
+    }
+}
